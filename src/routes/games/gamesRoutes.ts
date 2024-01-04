5f4982e276db209d6aadb6f0719a77749443530b
TangoCh4rlie <elouanreymond@tutanota.com>
2024-01-04 19:05:00 +0100

    ajout du middleware

--- src/routes/games/gamesRoutes.ts
+++ src/routes/games/gamesRoutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { getGamesByGameModeController, getGamesController, updateGameStateController } from "../../controllers/gamesController";
+import { ensureAuthenticated } from "../../middleware/loginMiddleware";
+import { isGameOwner } from "../../middleware/gameMiddleware";
 
 const router = express.Router();
 
 router.get('/', getGamesController);
 router.get('/mode', getGamesByGameModeController);
-router.put('/changeState', updateGameStateController);
+router.put('/changeState', ensureAuthenticated, isGameOwner, updateGameStateController);
 
 export default router;
