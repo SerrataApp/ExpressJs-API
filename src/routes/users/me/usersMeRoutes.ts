0d56638617b0393f4e3ac16dd5e45b6fd47223fc
TangoCh4rlie <loulou.reyms@orange.fr>
2024-01-08 20:50:50 +0100

    user update (email, uesrname)

--- src/routes/users/me/usersMeRoutes.ts
+++ src/routes/users/me/usersMeRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { deleteUserMeController, getUserMeController, updatePlayedGameController } from '../../../controllers/usersControllers/usersMeController';
+import { deleteUserMeController, getUserMeController, updatePlayedGameController, updatePlayerDataController } from '../../../controllers/usersControllers/usersMeController';
 
 const router = express.Router();
 
 router.get('/', getUserMeController);
 router.delete('/', deleteUserMeController);
+router.put('/', updatePlayerDataController);
 router.put('/game', updatePlayedGameController);
 
 export default router;
