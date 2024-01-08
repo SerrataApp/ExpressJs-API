4b49e6729bf87f171bcd22f87843eb4dd62dbab7
TangoCh4rlie <elouanreymond@tutanota.com>
2024-01-08 08:23:33 +0100

    suppression js

--- src/routes/admin/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var adminController_1 = require("../../controllers/adminControllers/adminController");
-var router = express_1.default.Router();
-router.put('/disable', adminController_1.disableUserController);
-router.delete('/game', adminController_1.deleteAnyGameController);
-router.delete('/user', adminController_1.deleteAnyUserController);
-exports.default = router;
--- src/routes/users/me/usersMeRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var usersMeController_1 = require("../../../controllers/usersControllers/usersMeController");
-var router = express_1.default.Router();
-router.get('/', usersMeController_1.getUserMeController);
-router.delete('/', usersMeController_1.deleteUserMeController);
-router.put('/game', usersMeController_1.updatePlayedGameController);
-exports.default = router;
