622a6ea8c9ac4ba8d9782ca15fb6c80737ab716a
TangoCh4rlie <loulou.reyms@orange.fr>
2024-01-09 19:21:15 +0100

    clc

--- src/tests/sample.test.ts
+++ src/tests/sample.test.ts
@@ -3,37 +3,37 @@ import { UserCreate, UserPrivateData, createUser, deleteUser } from "../models/u
 
 const prisma = new PrismaClient();
 
-describe('Test /users', () => {
-  describe('Create user', () => {
-    it('User should be OK', async () => {
-      // Create a mock UserCreate object for testing
-      const mockUserCreate = {
-        username: 'test123',
-        email: 'test123@example.com',
-        password: 'password123',
-        // Add other properties as needed for your UserCreate type
-      };
+// describe('Test /users', () => {
+//   describe('Create user', () => {
+//     it('User should be OK', async () => {
+//       // Create a mock UserCreate object for testing
+//       const mockUserCreate = {
+//         username: 'test123',
+//         email: 'test123@example.com',
+//         password: 'password123',
+//         // Add other properties as needed for your UserCreate type
+//       };
 
-      // Call the createUser function with the mockUserCreate object
-      let result: UserPrivateData | Boolean | null = await createUser(mockUserCreate as UserCreate);
+//       // Call the createUser function with the mockUserCreate object
+//       let result: UserPrivateData | Boolean | null = await createUser(mockUserCreate as UserCreate);
 
-      const test = {
-        //@ts-ignore
-        id: result.id,
-        //@ts-ignore
-        signupDate: result.signupDate,
-        admin: false,
-        cgu: false,
-        disabled: false,
-        email: "test123@example.com",
-        playedGames: 0,
-        username: "test123"
-      };
+//       const test = {
+//         //@ts-ignore
+//         id: result.id,
+//         //@ts-ignore
+//         signupDate: result.signupDate,
+//         admin: false,
+//         cgu: false,
+//         disabled: false,
+//         email: "test123@example.com",
+//         playedGames: 0,
+//         username: "test123"
+//       };
 
-      await deleteUser("test123");
+//       await deleteUser("test123");
 
-      expect(true).toEqual(true);
+//       expect(true).toEqual(true);
 
-    });
-  });
-});
+//     });
+//   });
+// });
