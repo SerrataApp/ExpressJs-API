3a5bf82d7b0a779865dd463b1fd80ede5b192759
TangoCh4rlie <elouanreymond@tutanota.com>
2024-01-09 18:50:08 +0100

    test marche pas

--- src/tests/sample.test.ts
+++ src/tests/sample.test.ts
@@ -1,32 +1,37 @@
 import { User } from "@prisma/client";
-import { createUser, deleteUser } from "../models/userModel"; // Update the path accordingly
+import { UserPrivateData, createUser, deleteUser } from "../models/userModel"; // Update the path accordingly
 
 describe('Test /users', () => {
   describe('Create user', () => {
     it('User should be OK', async () => {
       // Create a mock UserCreate object for testing
       const mockUserCreate = {
-        username: 'test',
-        email: 'test@example.com',
+        username: 'test123',
+        email: 'test123@example.com',
         password: 'password123',
         // Add other properties as needed for your UserCreate type
       };
 
       // Call the createUser function with the mockUserCreate object
-      const result = await createUser(mockUserCreate as User);
+      let result: UserPrivateData | Boolean | null = await createUser(mockUserCreate as User);
 
-      await deleteUser("test");
+      const { id, signupDate, ...expectedResult } = {
+        //@ts-ignore
+        id: result?.id,
+        //@ts-ignore
+        signupDate: result?.signupDate,
+        admin: false,
+        cgu: false,
+        disabled: false,
+        email: "test123@example.com",
+        playedGames: 0,
+        username: "test123"
+      };
+
+
+      await deleteUser("test123");
       // Add your assertions based on the expected behavior of the createUser function
-      expect(result).toEqual(
-        {"admin": false,
-        "cgu": false, 
-        "disabled": false, 
-        "email": "test@example.com", 
-        "id": 6, 
-        "playedGames": 0,
-        "signupDate": "2024-01-08T16:15:42.936Z", 
-        "username": "test"}
-      );
+      expect(result).toEqual(expectedResult);
 
     });
   });
